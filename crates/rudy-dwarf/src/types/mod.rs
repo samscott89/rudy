1c021eb0eb091378b1d50622455bf4fc80d5df2d
Sam Scott <sam.scott89@gmail.com>
2025-07-07 22:48:32 -0500

    Make all type resolution shallow (#31)

--- crates/rudy-dwarf/src/types/mod.rs
+++ crates/rudy-dwarf/src/types/mod.rs
@@ -4,7 +4,7 @@ mod index;
 mod resolution;
 
 pub use index::{find_type_by_name, get_die_typename, TypeIndexEntry};
-pub use resolution::{resolve_entry_type, resolve_type_offset, shallow_resolve_type};
+pub use resolution::{resolve_entry_type_shallow, resolve_type_offset, shallow_resolve_type};
 
 use crate::Die;
 
